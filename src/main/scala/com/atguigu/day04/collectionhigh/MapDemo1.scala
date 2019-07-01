8f35878138f57d90b720e215c403d765e9a6387e
John Doe <30752740@qq.com>
2019-07-01 17:31:01 +0800

    aaa

--- src/main/scala/com/atguigu/day04/collectionhigh/MapDemo1.scala
+++ src/main/scala/com/atguigu/day04/collectionhigh/MapDemo1.scala
@@ -6,6 +6,18 @@ package com.atguigu.day04.collectionhigh
   */
 object MapDemo1 {
     def main(args: Array[String]): Unit = {
-        
+        val list1 = List(30, 50, 70, 60, 10, 20)
+        // map使用特别的广阔  调整数据结构的类型
+        val result = list1.map(x => "a")
+        println(result)
     }
 }
+/*
+foreach
+
+map
+
+都会把函数应用在每个元素省,  map会有返回值, foreach没有返回值
+
+
+ */
--- src/main/scala/com/atguigu/day04/collectionhigh/PartitionDemo1.scala
+++ src/main/scala/com/atguigu/day04/collectionhigh/PartitionDemo1.scala
@@ -6,6 +6,19 @@ package com.atguigu.day04.collectionhigh
   */
 object PartitionDemo1 {
     def main(args: Array[String]): Unit = {
+        // 调用函数的时候只传入部分参数, 其他的暂时不传, 会返回一个新的函数, 这样的函数就叫部分应用函数
+        val pow2: Double => Double = math.pow(_,2)
+    
+        println(pow2(3))
+        println(pow2(4))
+        println(pow2(5))
         
+        val abs = math.abs(_:Double)
+        println(abs(-10))
     }
 }
+/*
+部分应用函数:
+ 
+
+ */
