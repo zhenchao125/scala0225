d914be19bbfb0c939cca3a32e5e4a0adf368b0cd
John Doe <30752740@qq.com>
2019-07-01 08:28:49 +0800

    更新

---
+++ src/main/scala/com/atguigu/day04/homework/Homework2.scala
@@ -0,0 +1,15 @@
+package com.atguigu.day04.homework
+
+/**
+  * Author lzc
+  * Date 2019-07-01 08:19
+  */
+object Homework2 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
+
+/*
+2. 定义一个 ! 操作符, 计算某个整数的阶乘. 比如 5! 应该得到 120   // 5 !
+ */
---
+++ src/main/scala/com/atguigu/day04/homework/Homework3.scala
@@ -0,0 +1,8 @@
+package com.atguigu.day04.homework
+
+object Homework3 {
+    
+}
+/*
+2. 定义一个 ! 操作符, 计算某个整数的阶乘. 比如 5! 应该得到 120   // 5 !
+ */
